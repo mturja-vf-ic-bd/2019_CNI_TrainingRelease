951de292d01cfacc64eb3de37ea025f5a51e44f4
mturja-vf-ic-bd <mturja.vf.ic.bd@gmail.com>
2019-07-30 11:08:00 -0400

    Trying Auto

--- data_driven_functional_connectivity/model.py
+++ data_driven_functional_connectivity/model.py
@@ -4,6 +4,11 @@ from torch.nn.parameter import Parameter
 import math
 
 
+def init_param(m):
+    if isinstance(m, nn.Conv1d) or isinstance(m, nn.BatchNorm1d):
+        m.reset_parameters()
+
+
 class FCModel(nn.Module):
     def __init__(self, n_nodes, n_sig):
         super(FCModel, self).__init__()
@@ -24,81 +29,70 @@ class FCModel(nn.Module):
 
 
 class NodeConv(nn.Module):
-    def __init__(self, n_sig, n1, n2, n3, n4, n5,
-                 kernel_size):
+    def __init__(self, channel_seq=[116, 20, 10],
+                 kernel_size=5, dropout=0.5):
         super(NodeConv, self).__init__()
-        self.n_sig = n_sig
-        self.conv_1 = nn.Conv1d(n1, n2, kernel_size, 1)
-        self.conv_2 = nn.Conv1d(n2, n3, kernel_size, 4)
-        self.conv_bn_1 = nn.BatchNorm1d(n3)
-        self.conv_3 = nn.Conv1d(n3, n4, kernel_size, 3)
-        self.conv_4 = nn.Conv1d(n4, 1, 9, 1)
-        self.conv_bn_2 = nn.BatchNorm1d(n4)
-        self.conv_5 = nn.Conv1d(n5, 1, 1, 1)
-        self.drop = nn.Dropout()
-        self.linear = nn.Linear(3, 1)
+        list_of_layer = []
+        for i in range(1, len(channel_seq)):
+            list_of_layer.append(
+                nn.Sequential(
+                    nn.Conv1d(channel_seq[i - 1], channel_seq[i], kernel_size, 1),
+                    nn.MaxPool1d(kernel_size),
+                    nn.LeakyReLU(0.1),
+                    nn.Dropout(dropout),
+                    nn.BatchNorm1d(channel_seq[i])
+                )
+            )
+        self.kernel_size = kernel_size
+        self.conv1d = nn.Sequential(*list_of_layer)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, in_sig):
-        out = self.conv_1(in_sig)
-        out = self.drop(out)
-        out = self.conv_2(out)
-        out = self.conv_bn_1(out)
-        out = self.drop(out)
-        out = self.conv_3(out)
-        out = self.conv_bn_2(out)
-        out = self.drop(out)
-        out = self.conv_4(out)
-        out = self.drop(out)
-        #out = self.conv_5(out)
-        # out = self.linear(out)
-        out = out.view(-1, 1)
-        out = self.sigmoid(out)
+        for i, layer in enumerate(self.conv1d):
+            if i == 0:
+                out = layer(in_sig)
+            else:
+                out = layer(out)
+        # out = self.sigmoid(out.view(-1, 1))
         return out
 
     def reset_parameters(self):
-        self.conv_1.reset_parameters()
-        self.conv_2.reset_parameters()
-        self.conv_3.reset_parameters()
-        self.conv_4.reset_parameters()
-        self.conv_5.reset_parameters()
-        self.conv_bn_1.reset_parameters()
-        self.conv_bn_2.reset_parameters()
+        for layer in self.conv1d:
+            if isinstance(layer, nn.Conv1d) or isinstance(layer, nn.BatchNorm1d):
+                layer.reset_parameters()
 
 
 class NodeRNN(nn.Module):
-    def __init__(self, n_sig, n_hidden, n_layer, n_nodes,
-                 kernel_size):
+    def __init__(self, feat_dim, conv1d_layers, n_hidden, n_layer,
+                 kernel_size, dropout):
         super(NodeRNN, self).__init__()
-        self.n_sig = n_sig
+        for i in range(len(conv1d_layers) - 1):
+            feat_dim = (feat_dim - (kernel_size - 1)) // kernel_size
         self.n_hidden = n_hidden
-        self.conv_1 = nn.Conv1d(n_nodes, n_nodes, kernel_size, 1)
-        self.conv_2 = nn.Conv1d(n_nodes, n_nodes, kernel_size, 2)
-        self.conv_bn = nn.BatchNorm1d(n_nodes)
-        self.drop1 = nn.Dropout()
-        self.maxpool = nn.MaxPool1d(4, stride=4)
-        self.gru = nn.GRU(17, n_hidden, n_layer,
+        self.conv1d = NodeConv(conv1d_layers, kernel_size, dropout)
+        self.gru = nn.GRU(feat_dim, n_hidden, n_layer,
                           bidirectional=True, batch_first=True)
-        self.gru_bn = nn.BatchNorm1d(2 * n_hidden)
-        self.drop2 = nn.Dropout()
-        self.dense = nn.Linear(2 * n_hidden, 1)
-        self.sigmoid = nn.Sigmoid()
+        self.non_linear = nn.Sequential(
+            nn.LeakyReLU(0.1),
+            nn.Dropout(dropout),
+            nn.BatchNorm1d(2 * n_hidden)
+        )
+        self.dense = nn.Sequential(nn.Linear(2 * n_hidden, 1) , nn.Sigmoid())
 
     def forward(self, in_sig):
-        out = self.conv_1(in_sig)
-        out = self.conv_2(out)
-        out = self.conv_bn(out)
-        out = self.drop1(out)
-        out = self.maxpool(out)
+        out = self.conv1d(in_sig)
         out, h = self.gru(out)
         out = torch.cat((out[:, -1, :self.n_hidden], out[:, 0, self.n_hidden:]), 1)
-        out = self.gru_bn(out)
-        out = self.drop2(out)
-        out = self.sigmoid(self.dense(out))
+        out = self.non_linear(out)
+        out = self.dense(out)
         return out
 
     def reset_parameters(self):
-        self.conv_1.reset_parameters()
-        self.conv_2.reset_parameters()
+        self.conv1d.reset_parameters()
         self.gru.reset_parameters()
-        self.dense.reset_parameters()
+        for layer in self.non_linear:
+            if isinstance(layer, nn.BatchNorm1d):
+                layer.reset_parameters()
+        for layer in self.dense:
+            if isinstance(layer, nn.Linear):
+                layer.reset_parameters()
--- train_rnn.py
+++ train_rnn.py
@@ -6,9 +6,10 @@ import numpy as np
 
 import torch
 import torch.optim as optim
+import torch.nn as nn
 import time
 from parser import load_data
-from data_driven_functional_connectivity.model import NodeRNN, NodeConv
+from data_driven_functional_connectivity.model import NodeRNN, NodeConv, init_param
 from util import accuracy
 
 # Training settings
@@ -18,17 +19,17 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
 parser.add_argument('--fastmode', action='store_true', default=False,
                     help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=10,
+parser.add_argument('--epochs', type=int, default=20,
                     help='Number of epochs to train.')
-parser.add_argument('--lr', type=float, default=0.007,
+parser.add_argument('--lr', type=float, default=0.001,
                     help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-4,
                     help='Weight decay (L2 loss on parameters).')
-parser.add_argument('--hidden', type=int, default=128,
+parser.add_argument('--hidden', type=int, default=256,
                     help='Number of hidden units.')
-parser.add_argument('--dropout', type=float, default=0.5,
+parser.add_argument('--dropout', type=float, default=0.7,
                     help='Dropout rate (1 - keep probability).')
-parser.add_argument('--kernel_size', type=int, default=10,
+parser.add_argument('--kernel_size', type=int, default=5,
                     help='kernel size in 1d conv layer')
 
 args = parser.parse_args()
@@ -45,10 +46,9 @@ train_idx = torch.LongTensor(train_idx)
 test_idx = torch.LongTensor(test_idx)
 
 # Model and optimizer
-# model = NodeRNN(n_sig=data.size(2), n_hidden=args.hidden, n_layer=1, n_nodes=data.size(1), kernel_size=args.kernel_size)
-model = NodeConv(n_sig=data.size(2), n1=data.size(1), n2=40, n3=20, n4=10, n5=5, kernel_size=args.kernel_size)
-optimizer = optim.Adam(model.parameters(),
-                       lr=args.lr, weight_decay=args.weight_decay)
+model = NodeRNN(data.size(2), [data.size(1), 20], args.hidden, 1, args.kernel_size, args.dropout)
+# model = NodeConv([data.size(1), 50, 40, 20, 10, 5, 1], kernel_size=2)
+optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
 if args.cuda:
     model = model.cuda()
@@ -70,7 +70,7 @@ def train(epoch, train_idx, test_idx):
     optimizer.step()
     if epoch%5 == 0:
         print('epoch: {},  loss: {}'.format(epoch, loss.data))
-    return tst_rnn(test_idx)
+    return tst_rnn(train_idx), tst_rnn(test_idx)
 
     # if not args.fastmode:
     #     # Evaluate validation set performance separately,
@@ -97,21 +97,31 @@ def tst_rnn(idx_test):
     return acc
 
 
+def weight_init(m):
+    if isinstance(m, nn.Conv1d) or isinstance(m, nn.BatchNorm1d) \
+            or isinstance(m, nn.Linear) or isinstance(m, nn.GRU):
+        m.reset_parameters()
+
 # Train model
 t_total = time.time()
 acc = []
 from matplotlib import pyplot as plt
 acc_batch = np.zeros(args.epochs)
+acc_tr = []
 for i in range(len(test_idx)):
-    model.reset_parameters()
+    model.apply(weight_init)
     acc_train = []
+    acc_test = []
     for epoch in range(args.epochs):
-        acc_train.append(train(epoch, train_idx[i], test_idx[i]))
-    tst_rnn(train_idx[i])
+        a, b = train(epoch, train_idx[i], test_idx[i])
+        acc_train.append(a)
+        acc_test.append(b)
+
     acc.append(tst_rnn(test_idx[i]))
-    acc_batch = acc_batch + np.array(acc_train)
-    plt.plot(acc_train)
+    plt.plot(acc_train, "r-")
+    plt.plot(acc_test, "b")
     plt.show()
+    acc_batch = acc_batch + np.array(acc_test)
 acc_batch /= len(test_idx)
 print(acc_batch)
 plt.plot(acc_batch)
