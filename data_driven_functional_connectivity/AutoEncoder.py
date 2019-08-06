23ffed355eda8d9d5bfc23d42ea263f65d6b00d4
mturja-vf-ic-bd <mturja.vf.ic.bd@gmail.com>
2019-08-06 14:02:12 -0400

    Autoencoder running

--- data_driven_functional_connectivity/AutoEncoder.py
+++ data_driven_functional_connectivity/AutoEncoder.py
@@ -17,8 +17,6 @@ class EncoderRNN(nn.Module):
         self.gru_sigma = nn.GRU(feat_dim, n_hidden, n_layer,
                              bidirectional=True, batch_first=True)
         self.dropout = nn.Dropout(dropout)
-        self.linear = nn.Linear(2*n_hidden, 1)
-        self.sigmoid = nn.Sigmoid()
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
@@ -35,8 +33,7 @@ class EncoderRNN(nn.Module):
         out_sigma = torch.cat((out_sigma[:, -1, :self.n_hidden], out_sigma[:, 0, self.n_hidden:]), 1)
 
         out = self.reparameterize(out_mu, out_sigma)
-        out = self.sigmoid(self.linear(out))
-        return out
+        return out, out_mu, out_sigma
 
     def reset_parameters(self):
         self.conv1d.reset_parameters()
@@ -51,7 +48,7 @@ class DecoderRNN(nn.Module):
         self.hidden_dim = hidden_dim
         self.n_layer = n_layer
         self.dropout = dropout
-        self.gru = nn.GRU(input_dim, hidden_dim, n_layer, bidirectional=True, batch_first=True)
+        self.gru = nn.GRU(input_dim, hidden_dim, n_layer, bidirectional=False, batch_first=True)
 
     def forward(self, input, hidden):
         out, hidden = self.gru(input, hidden)
@@ -59,13 +56,32 @@ class DecoderRNN(nn.Module):
 
 
 class SigAutoEncoder(nn.Module):
-    def __init__(self, input_dim, conv1_layers, kernel_size, hidden_enc, hidden_dec, dropout, seq_len):
+    def __init__(self, input_dim, conv1_layers, kernel_size, hidden_enc, hidden_dec, dropout):
         super(SigAutoEncoder, self).__init__()
+        self.input_dim = input_dim
         self.encoder = EncoderRNN(input_dim, conv1_layers, hidden_enc, n_layer=1, kernel_size=kernel_size, dropout=dropout)
         self.decoder = DecoderRNN(input_dim, hidden_dec, n_layer=1, dropout=dropout)
+        self.linear = nn.Linear(hidden_dec, input_dim)
 
     def forward(self, input):
-        z = self.encoder(input)
-        input = torch.zeros()
-        for i in range(1, self.seq_len):
-            out[i]
+        batch_size = input.size(0)
+        seq_len = input.size(2)
+        z, mu, logvar = self.encoder(input)
+        input = input.squeeze()
+        reconstructed = torch.zeros(batch_size, seq_len, self.input_dim).cuda()
+
+        for i in range(0, seq_len):
+            if i == 0:
+                # inp = input[i+1:]
+                inp = input[:, -1].unsqueeze(1)
+                _, hidden = self.decoder(inp, z.unsqueeze(0))
+            else:
+                # inp = torch.cat((input[:, 0:i], input[:, i+1:]), 1)
+                inp = input[:, i-1].unsqueeze(1)
+                _, hidden = self.decoder(inp, hidden)
+            output = self.linear(hidden.squeeze())
+            reconstructed[:, i] = output
+        return reconstructed, mu, logvar
+        # input = torch.zeros()
+        # for i in range(1, self.seq_len):
+        #     out[i]
--- train_rnn.py
+++ train_rnn.py
@@ -10,8 +10,8 @@ import torch.nn as nn
 import time
 from parser import load_data
 from data_driven_functional_connectivity.model import NodeRNN, NodeConv, init_param, SAE
-from util import accuracy, pca_dim_red
-from data_driven_functional_connectivity.AutoEncoder import EncoderRNN
+from util import accuracy, pca_dim_red, plot_signal
+from data_driven_functional_connectivity.AutoEncoder import EncoderRNN, SigAutoEncoder
 
 # Training settings
 parser = argparse.ArgumentParser()
@@ -22,15 +22,15 @@ parser.add_argument('--fastmode', action='store_true', default=False,
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
 parser.add_argument('--epochs', type=int, default=500,
                     help='Number of epochs to train.')
-parser.add_argument('--lr', type=float, default=0.0001,
+parser.add_argument('--lr', type=float, default=0.01,
                     help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-4,
                     help='Weight decay (L2 loss on parameters).')
-parser.add_argument('--hidden', type=int, default=256,
+parser.add_argument('--hidden', type=int, default=128,
                     help='Number of hidden units.')
 parser.add_argument('--dropout', type=float, default=0.1,
                     help='Dropout rate (1 - keep probability).')
-parser.add_argument('--kernel_size', type=int, default=5,
+parser.add_argument('--kernel_size', type=int, default=3,
                     help='kernel size in 1d conv layer')
 
 args = parser.parse_args()
@@ -48,12 +48,13 @@ train_idx = torch.LongTensor(train_idx)
 test_idx = torch.LongTensor(test_idx)
 
 # Model and optimizer
-model = NodeRNN(data.size(3), [5, 1], args.hidden, 1, 2, args.dropout)
+# model = NodeRNN(data.size(3), [5, 1], args.hidden, 1, 2, args.dropout)
 # model = NodeConv([data.size(1), 30, 5, 1], kernel_size=5, dropout=args.dropout, sigmoid=True)
 # model = SAE(conv_seq=[data.size(1), 50, 20], conv_kernel=10,
 #             deconv_seq=[20, 50, data.size(1)], deconv_kernel=5,
 #             deconv_stride=[5, 6], dropout=0.6)
 # model = EncoderRNN(data.size(3), [5, 1], args.hidden, 1, 2, args.dropout)
+model = SigAutoEncoder(data.size(3), [5, 1], args.kernel_size, args.hidden, args.hidden*2, args.dropout)
 optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
 if args.cuda:
@@ -68,17 +69,23 @@ if args.cuda:
 def train(epoch, train_idx, test_idx):
     model.train()
     optimizer.zero_grad()
+    input = data[train_idx]
+    # input = input[0:10]
 
-    output = model(data[train_idx])
-    # loss_fn = torch.nn.MSELoss()
-    # loss = loss_fn(output, data[train_idx])
-    loss_fn = torch.nn.BCELoss()
-    loss = loss_fn(output, label[train_idx])
-    loss.backward()
+    output, mu, logvar = model(input)
+
+    loss_fn = torch.nn.MSELoss()
+    loss = 100*loss_fn(output, input) - 0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    # loss_fn = torch.nn.BCELoss()
+    # loss = loss_fn(output, label[train_idx])
+    # loss.backward()
     optimizer.step()
+
+    plot_signal(output.detach().numpy()[0:3])
     if epoch%5 == 0:
         print('epoch: {},  loss: {}'.format(epoch, loss.data))
-    return tst_rnn(train_idx), tst_rnn(test_idx), loss.data
+    # return tst_rnn(train_idx), tst_rnn(test_idx), loss.data
+    return loss.data
 
     # if not args.fastmode:
     #     # Evaluate validation set performance separately,
@@ -122,20 +129,21 @@ for i in range(len(test_idx)):
     acc_test = []
     loss_list = []
     for epoch in range(args.epochs):
-        a, b, loss = train(epoch, train_idx[i], test_idx[i])
-        acc_train.append(a)
-        acc_test.append(b)
+        # a, b, loss = train(epoch, train_idx[i], test_idx[i])
+        loss = train(epoch, train_idx[i], test_idx[i])
+        # acc_train.append(a)
+        # acc_test.append(b)
         loss_list.append(loss)
 
-    acc.append(tst_rnn(test_idx[i]))
-    plt.plot(acc_train, "r-")
-    plt.plot(acc_test, "b")
+    # acc.append(tst_rnn(test_idx[i]))
+    # plt.plot(acc_train, "r-")
+    # plt.plot(acc_test, "b")
     plt.plot(loss_list)
     plt.show()
-    acc_batch = acc_batch + np.array(acc_test)
-acc_batch /= len(test_idx)
+    # acc_batch = acc_batch + np.array(acc_test)
+# acc_batch /= len(test_idx)
 print(loss_list)
-plt.plot(acc_batch)
+# plt.plot(acc_batch)
 plt.show()
 
 
